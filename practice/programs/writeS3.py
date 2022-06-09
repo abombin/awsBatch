51afd5579ae9baf945d2c9e13a6d960b6a424dea
abombin <abombin93@gmail.com>
2022-06-09 21:15:00 +0000

    made write  a function

--- practice/programs/writeS3.py
+++ practice/programs/writeS3.py
@@ -2,10 +2,12 @@ import os
 import boto3
 import pandas as pd
 from io import StringIO
-#import s3fs
+
 
 
 s3 = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+
 
 def bucketList():
     response = s3.list_buckets()
@@ -18,21 +20,21 @@ def bucketList():
 
 #print(os.getcwd())
 
-
 # read data from the server
-df=pd.read_csv('/home/ubuntu/github/shinyApp/shinyVizApps/uploadDatCov/data/metaData.tsv', sep='\t')
+myDf=pd.read_csv('/home/ubuntu/github/shinyApp/shinyVizApps/uploadDatCov/data/metaData.tsv', sep='\t')
 
 #print(df.columns)
 
 # write data to s3
-bucket = 'abombin' 
-csv_buffer = StringIO()
-df.to_csv(csv_buffer, index=False)
-s3_resource = boto3.resource('s3')
-s3_resource.Object(bucket, 'df.csv').put(Body=csv_buffer.getvalue())
+myBucket = 'abombin' 
+
+def writeToS3(bucket, df):
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3_resource.Object(bucket, 'metaDat1.csv').put(Body=csv_buffer.getvalue())
+
+bucketList()
+
+writeToS3(myBucket, myDf)
 
 
-#df.to_csv(
-#    "s3://abombin/metaDat.csv",
-#    index=False,
-#)
